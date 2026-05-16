8f229817d5a6aef61643b21d7ce5125fea51be71
afsan <afsan>
2026-05-16 17:49:11 -0400

    Added ImGui Region for code organization

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,15 @@ int main()
 		TraceLog(LOG_ERROR, "Couldn't Initialize raylib Window.");
 	}
 
+	#pragma region ImGui
+		rlImGuiSetup(true);
 
-	rlImGuiSetup(true);
-
-	ImGuiIO &io = ImGui::GetIO();
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+		ImGuiIO &io = ImGui::GetIO();
+		//io.ConfigFlags | ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Control
+		//io.ConfigFlags | ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Control
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable Docking.
+		io.FontGlobalScale = 2.5; // Adjust Font scaling.
+	#pragma endregion
 
 	SetTargetFPS(60);
 	int x_pos = 30;
@@ -44,17 +48,35 @@ int main()
 		DrawRectangle(75, 75, size, size, { 0, 255, 0, 255 });
 		DrawRectangle(50, 50, size, size, {255, 0, 0, 255});
 		
-
+		/*
+			ImGui test window 1
+		*/
 		ImGui::Begin("Test");
-
 		ImGui::Text("Hello World");
-
 		if (ImGui::Button("Test Buttton"))
 		{
-			std::cout << "Pressed Button" << std::endl;
+			std::cout << "Pressed Test Button 1" << std::endl;
 		}
+		ImGui::SameLine();
+		ImGui::PushID(2);
+		if (ImGui::Button("Test Button"))
+		{
+			TraceLog(LOG_DEBUG, "Pressed Test Button 2");
+		}
+		ImGui::PopID();
+		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		/*
+			ImGui test window 2
+		*/
+
+		ImGui::Begin("New Testing Window");
+		
+		ImGui::Text("Hello again.");
+		ImGui::Separator();
+		ImGui::NewLine();
+		static float a = 0.0;
+		ImGui::SliderFloat("slider", &a, 0, 1);
 
 		ImGui::End();
 
