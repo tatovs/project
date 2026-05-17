8cda50215cc46f72ed3c821bcc5585c7bdec1ee9
afsan <afsan>
2026-05-17 16:54:20 -0400

    Implemented delta time for basic movement

--- src/game_layer/game_main.cpp
+++ src/game_layer/game_main.cpp
@@ -4,6 +4,12 @@
 
 #include "game_main.h"
 
+struct Game_Data
+{
+	float x_position = 100.0f;
+	float y_position = 100.0f;
+}game_data;
+
 bool init_game()
 {
 	return true;
@@ -11,13 +17,16 @@ bool init_game()
 
 bool update_game()
 {
-	Color c;
-	c.r = 255;
-	c.g = 0;
-	c.b = 200;
-	c.a = 255;
+	float delta_time = GetFrameTime();
+	if(delta_time > 1.f / 5) {delta_time = 1 / 5.f;}
 
-	DrawText("Congrats! You created your first window!", 190, 200, 20, c);
+	if (IsKeyDown(KEY_A)) {game_data.x_position -= 200 * delta_time;}
+	if (IsKeyDown(KEY_D)) {game_data.x_position += 200 * delta_time;}
+	if (IsKeyDown(KEY_W)) {game_data.y_position -= 200 * delta_time;}
+	if (IsKeyDown(KEY_S)) {game_data.y_position += 200 * delta_time;}
+
+	DrawRectangle(game_data.x_position, game_data.y_position, 50, 50, {255, 0, 200, 255});
+
 	return true;
 }
 
