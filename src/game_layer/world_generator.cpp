31146c7118a1b4de6761c9c268e8680b36a1e72c
afsan <afsan>
2026-05-26 13:18:18 -0400

    Added FastNoiseSIMD library and started implementing for world gen

--- src/game_layer/world_generator.cpp
+++ src/game_layer/world_generator.cpp
@@ -1,6 +1,8 @@
 #include "world_generator.h"
 #include "rng.h"
 
+#include <FastNoiseSIMD.h>
+
 
 void generate_world(Game_Map &game_map, int seed)
 {
@@ -9,8 +11,28 @@ void generate_world(Game_Map &game_map, int seed)
 
 	game_map.create(width, height);
 
-	std::ranlux24_base rng(seed);
+	std::ranlux24_base rng(seed++);
 
+	std::unique_ptr<FastNoiseSIMD> dirt_noise_generator(FastNoiseSIMD::NewFastNoiseSIMD());
+	std::unique_ptr<FastNoiseSIMD> stone_noise_generator(FastNoiseSIMD::NewFastNoiseSIMD());
+
+	dirt_noise_generator->SetSeed(seed++);
+	stone_noise_generator->SetSeed(seed++);
+
+	dirt_noise_generator->SetNoiseType(FastNoiseSIMD::NoiseType::SimplexFractal);
+	dirt_noise_generator->SetFractalOctaves(1);
+	dirt_noise_generator->SetFrequency(0.02);
+	
+	stone_noise_generator->SetNoiseType(FastNoiseSIMD::NoiseType::SimplexFractal);
+	stone_noise_generator->SetFractalOctaves(4);
+	stone_noise_generator->SetFrequency(0.01);
+
+	float *dirt_noise = FastNoiseSIMD::GetEmptySet(width);
+	float *stone_noise = FastNoiseSIMD::GetEmptySet(width);
+
+	dirt_noise_generator->FillNoiseSet(dirt_noise, 0, 0, 0, width, 1, 1);
+	stone_noise_generator->FillNoiseSet(stone_noise, 0, 0, 0, width, 1, 1);
+	/*
 	int dirt_change_cooldown = get_random_int(rng, 5, 35);
 	int dirt_change_direction = get_random_int(rng, -3, 3);
 
@@ -131,4 +153,7 @@ void generate_world(Game_Map &game_map, int seed)
 			game_map.get_block_unsafe(x, y, Game_Map::Layer::map) = b;
 		}
 	}
+	*/
+	FastNoiseSIMD::FreeNoiseSet(dirt_noise);
+	FastNoiseSIMD::FreeNoiseSet(stone_noise);
 }
